abf80d22ad41fb4a29c6fe8cb16f274bec1216ea
Alexis Destrez <alexis.destrez@schibsted.com>
2019-05-23 16:01:36 +0200

    Handle compression and cgo environment variable

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,23 +18,30 @@ build_ldflags_xvar() {
 
 build_ldflags() {
     local name="$1"
+    local -r compress="$2"
     local -r pkg="internal/pkg/app"
-    local -a xflags
+    local -a ldflags
 
     if [[ "$name" != "$(project_name)" ]]; then
         name="$(project_name)_$name"
     fi
 
-    xflags+=("$(build_ldflags_xvar "$pkg" "name" "$name")")
-    xflags+=("$(build_ldflags_xvar "$pkg" "builtAtRaw" "$(date -u +'%Y-%m-%dT%H:%M:%SZ')")")
-    xflags+=("$(build_ldflags_xvar "$pkg" "version" "$(git describe --tags --always --dirty="-dev" 2>/dev/null || echo "0.0.0-0-gmaster")")")
+    if [ "$compress" -eq 1 ]; then
+        ldflags+=("-w")
+        ldflags+=("-s")
+    fi
 
-   echo "$(IFS=' ' ; echo "${xflags[*]}")"
+    ldflags+=("$(build_ldflags_xvar "$pkg" "name" "$name")")
+    ldflags+=("$(build_ldflags_xvar "$pkg" "builtAtRaw" "$(date -u +'%Y-%m-%dT%H:%M:%SZ')")")
+    ldflags+=("$(build_ldflags_xvar "$pkg" "version" "$(git describe --tags --always --dirty="-dev" 2>/dev/null || echo "0.0.0-0-gmaster")")")
+
+   echo "$(IFS=' ' ; echo "${ldflags[*]}")"
 }
 
 build() {
     local -r project_to_build="$1"
     local -r project_to_build_path="$(project_path_cmd)/${project_to_build}"
+    local -r compress="${BUILD_COMPRESS:-0}"
 
     # test if the project exists
     test -d "$project_to_build_path" || exit_error 1 "project '${project_to_build}' not found"
@@ -46,13 +53,13 @@ build() {
     go generate ./...
 
     # build the binary
-    GOOS="${BUILD_FOR_OS:-$(go env GOOS)}" GOARCH="${BUILD_FOR_ARCH:-$(go env GOARCH)}" CGO_ENABLED=0 \
+    GOOS="${BUILD_FOR_OS:-$(go env GOOS)}" GOARCH="${BUILD_FOR_ARCH:-$(go env GOARCH)}" CGO_ENABLED="${BUILD_WITH_CGO:=0}" \
     go build -v \
         -o "$(project_path_build_bin)/${project_to_build}" \
-        -ldflags="-s -w $(build_ldflags "$project_to_build")" \
+        -ldflags="$(build_ldflags "$project_to_build" "$compress")" \
         "$project_to_build_path"
 
-    if [ "${BUILD_COMPRESS:-0}" -eq 1 ]; then
+    if [ "$compress" -eq 1 ]; then
         upx --brute "$(project_path_build_bin)/${project_to_build}"
     fi
 }
